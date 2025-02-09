b671fa91a7e3e41f52663cec7bb930f4836f64a3
jettinsha <jettinsha@gmail.com>
2025-02-09 18:33:06 +0530

    integrate Firebase and add necessary plugins for cross-platform support

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:listing_app/pages/homepage.dart';
 import 'package:listing_app/pages/loginpage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
